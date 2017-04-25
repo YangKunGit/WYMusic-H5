adaf1ed381bff17b53d55c6f7209a4027cef4e8b
YK <apple@ssd-china.cn>
2017-04-25 17:24:55 +0800

    评论列表和分页

--- js/play.js
+++ js/play.js
@@ -1,3 +1,27 @@
 /**
  * Created by YK on 17/4/18.
  */
+
+//    常参
+var PLAY_MODE_NAME = ['单曲循环', '顺序播放', '随机播放']
+var SINGLE = 0
+var SEQUENTIAL = 1
+var RANDOM = 2
+var DEFAULT_IMG = '../assets/Vue_Music_Blur.png'
+var DEFAULT_SONG_NAME = 'VUE MUSIC'
+var DEFAULT_SINGER_NAME = 'YANG KUN'
+
+
+//  变量
+var playing = false                         //  是否在播放
+var currentTime = 0                         //  当前播放进度
+var duration = 0                            //  歌曲总时长
+var playMode = SEQUENTIAL                   //  播放模式
+var index = 0                               //  播放列表中的下标
+var song = {
+    name: DEFAULT_SONG_NAME,                //  歌曲名
+    singer: DEFAULT_SINGER_NAME,            //  歌手
+    coverImgUrl: DEFAULT_IMG                //  歌曲图片
+}
+var playList = []                           //  播放列表
+
